60e8372efab8549a65f749de07295952355c5475
Asier Garcia Ruiz <asier.garciaruiz@gmail.com>
2021-09-17 15:53:13 -0400

    Algo as written in book

--- math4640/math4640-code/src/lib.rs
+++ math4640/math4640-code/src/lib.rs
@@ -1,39 +1,38 @@
-use ndarrary::prelude::*;
+use ndarray::{ArrayBase, Array1, Array2}; 
 
-fn gaussian_elimination(A: ndarray::arr2 , b: ndarrary::arr1) -> Vec<f64> {
-    let n = A.Dim().0; // We assume A is square
-    let mut M = Array::zeros((n,n)); // Multiplier matrix
+fn gaussian_elimination(mut A: Array2<f64>, b: Array1<f64>) -> Vec<f64> {
+    let n = A.shape()[0]; // We assume A is square
+    let mut M: Array2<f64> = ArrayBase::zeros((n,n)); // Multiplier matrix
     
-    for k in (1..n) {
-        if A[k][k] == 0 {
+    for k in 1..n {
+        if A[[k,k]] == 0.0 {
             panic!("Zero pivot encountered!");
         }
-        for i in (k+1..n) {
-            M[i][k] = A[i][k] / A[k][k];
+        for i in k+1..n {
+            M[[i,k]] = A[[i,k]] / A[[k,k]];
         }
-        for j in (k=1..n) {
-            for i in (k=1..n) {
-                A[i][j] = A[i][j] - M[i][k] * A[k][j];
+        for j in k+1..n {
+            for i in k+1..n {
+                A[[i,j]] = A[[i,j]] - M[[i,j]] * A[[k,j]];
             }
         }
     }
-    println!("{A}");
-    println!("{M}");
+    println!{"{}", A};
     vec!(0.0)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*
+    use super::*;
+    use ndarray::array;
 
     #[test]
     fn test_gaussian_elimination() {
-        let A = array![[1.0, 1/2, 1/3],
-                        [1/2, 1/3, 1/4],
-                        [1/3, 1/4, 1/5]];
-        let b = array![7/6, 5/6, 13/20];
+        let A = array![[1.0, 1.0/2.0, 1.0/3.0],
+                        [1.0/2.0, 1.0/3.0, 1.0/4.0],
+                        [1.0/3.0, 1.0/4.0, 1.0/5.0]];
+        let b = array![7.0/6.0, 5.0/6.0, 13.0/20.0];
         
         gaussian_elimination(A, b);
-
     }
 }
